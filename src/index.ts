21d020ff6f889ce0db8f7d36e2fda6c071ee431e
Genki Sato <genki.sato@lasca.co.jp>
2021-10-04 15:52:25 +0900

    template copy

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import fs from "fs-extra";
+import path from "path";
+
 module.exports = () => {
-  console.log("cli test");
+  console.log(process.cwd());
+  console.log(__dirname);
+
+  fs.copySync(path.resolve(__dirname, "template/lasca-app"), "lasca-app");
 };
